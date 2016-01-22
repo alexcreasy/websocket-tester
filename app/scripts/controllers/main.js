53ce51e55c342e284156d3705f1b6435b4484d1f
Alex Creasy <acreasy@redhat.com>
2016-01-22 15:05:05 +0000

    Initial scaffold

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name websocketTesterApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the websocketTesterApp
+ */
+angular.module('websocketTesterApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
